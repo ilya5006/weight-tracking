4983d3d948224cb382bdcc85d70957dddfb85aaa
ilya5006 <ilya.samarczew2011@yandex.ru>
2020-03-22 16:00:59 +0300

    Users weights

--- model/php/get-all-users-weights.php
+++ model/php/get-all-users-weights.php
@@ -3,9 +3,23 @@
 
     SESSION_START();
 
-    $username = isset($_REQUEST['username']) ? $db->escapeString($_REQUEST['username']) : $_SESSION['username'];
+    $choseUser = isset($_REQUEST['username']) ? $db->escapeString($_REQUEST['username']) : $_SESSION['username'];
 
-    $users = $db->queryAll("SELECT DISTINCT `username`, `weight` FROM `weights` WHERE `username` != '$username' ORDER BY id DESC");
+    $usersInfo = [];
+
+    $usernames = $db->queryAll("SELECT DISTINCT `username` FROM `weights` WHERE username != '$choseUser'");
+
+    foreach ($usernames as $username)
+    {
+        $username = $username['username'];
+
+        $userWeight = $db->query("SELECT `weight` FROM `weights` WHERE `username` = '$username' ORDER BY id DESC");
+
+        $userWeight = $userWeight['weight'];
+
+        $usersInfo[] = ['username' => $username, 'weight' => $userWeight];
+
+    }
     
-    echo json_encode($users, JSON_NUMERIC_CHECK);
+    echo json_encode($usersInfo, JSON_NUMERIC_CHECK | JSON_UNESCAPED_UNICODE);
 ?>
